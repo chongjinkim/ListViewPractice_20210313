a26e4e582de130ad34b1f8b0a2653171ac7af748
김종진 <martia0118@gmail.com>
2021-03-20 10:09:11 +0900

    inflater 변수 추가

--- app/src/main/java/com/nepplus/listviewpractice_20210313/adaptors/StudentAdaptor.kt
+++ app/src/main/java/com/nepplus/listviewpractice_20210313/adaptors/StudentAdaptor.kt
@@ -1,12 +1,18 @@
 package com.nepplus.listviewpractice_20210313.adaptors
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.widget.ArrayAdapter
 import com.nepplus.listviewpractice_20210313.datas.Student
 
 
 class StudentAdaptor(
-                     val mContext : Context,
-                     val resId : Int,
-                     val mList : ArrayList<Student>) : ArrayAdapter<Student>(mContext, resId, mList){
+      val mContext : Context,//어떤 화면에서 쓸것인가
+      val resId : Int,
+      val mList : ArrayList<Student>) : ArrayAdapter<Student>(mContext, resId, mList){
+
+//xml -> 코틀린 변수로 꺼내오도록 도와주는 클래스(LayoutFlater)
+        val inflater = LayoutInflater.from(mContext)
+
+
 }
