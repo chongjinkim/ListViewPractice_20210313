f1e7e20513e1328ac54db9f535a82c13d22d1979
김종진 <martia0118@gmail.com>
2021-03-20 11:42:48 +0900

    리스트뷰 클릭 이벤트 구현 예제

--- app/src/main/java/com/nepplus/listviewpractice_20210313/MainActivity.kt
+++ app/src/main/java/com/nepplus/listviewpractice_20210313/MainActivity.kt
@@ -2,6 +2,7 @@ package com.nepplus.listviewpractice_20210313
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.nepplus.listviewpractice_20210313.adaptors.StudentAdaptor
 import com.nepplus.listviewpractice_20210313.datas.Student
 import kotlinx.android.synthetic.main.activity_main.*
@@ -36,5 +37,18 @@ class MainActivity : AppCompatActivity() {
 //      완성된 어댑터 변수를 리스트 뷰와 연결
 
         studentListView.adapter = mAdaptor
+
+//      학생 리스트뷰의 클릭 이벤트 구현
+
+        studentListView.setOnItemClickListener { parent, view, position, id ->
+
+//      position : 몇 번 줄이 눌렀는지 알려주는 역할
+
+//        클릭 된 학생의 이름을 토스트로 출력
+
+            val clickedStudent = mStudentList[position]
+
+            Toast.makeText(this, clickedStudent.name, Toast.LENGTH_SHORT).show()
+        }
     }
 }
